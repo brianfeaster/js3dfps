34638d7022293960fe4b7bd2c8ca36c45889ac95
Brian Feaster <brianf@gmail.com>
2018-09-09 04:18:28 +0000

    New engine development.

--- server.js
+++ server.js
@@ -7,7 +7,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 const Util=require('util');
 const Crypto=require('crypto');
-const Log = console.log;
+const Log = process.stdout.write.bind(process.stdout); //console.log;
+String.prototype.hex = function(){ return [...this].map(c=>("0"+c.charCodeAt().toString(16)).slice(-2)).join(); }
+const red   ='\x1b[31m'
+const yellow='\x1b[33m'
+const off   ='\x1b[0m'
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -36,9 +41,24 @@ const DB = (function () {
 ////////////////////////////////////////////////////////////////////////////////
 // Server
 ////////////////////////////////////////////////////////////////////////////////
-const Net=require('net');
-const qserver = Net.createServer(receiveConnection);
-qserver.listen(7199);
+function server () {
+  const Net=require('net');
+  const qserver = Net.createServer(receiveConnection);
+  qserver.listen(7199);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Agents
+////////////////////////////////////////////////////////////////////////////////
+function Agent (s, id) { // id socket
+  this.id      = id;
+  this.sock    = s;
+  this.buffer  = Buffer(0);
+  this.headers = {};
+  this.state   = 0;
+  this.yeld    = false;
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -46,89 +66,86 @@ qserver.listen(7199);
 ////////////////////////////////////////////////////////////////////////////////
 let connectionCount= 0;
 
-function receiveConnection (s) { // socket
-  var count = ++connectionCount;
-  s.on('data', receiveBuffer.bind(null,count,s));
+function receiveConnection (sock) {
+  sock.on('data', receiveBuffer.bind(new Agent(sock, ++connectionCount)));
+  // TODO notify other clients new player.  Notify player of all current players.
 };
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// Agent
+// State Machine
 ////////////////////////////////////////////////////////////////////////////////
-var buffers = new Buffer(0);
-var httpHeaders = {};
 
-function receiveBuffer (id, s, b) { // number socket Buffer
-  //DB("\x1b[33m" + JSON.stringify(b.toString()) + "\x1b[0m");
-  buffers = Buffer.concat([buffers,b]);
-  //DB(buffers)
-  consume(id, s)
+function receiveBuffer (b) { // Buffer
+  var agent = this; // Cheating
+  var id = agent.id;
+  var s = agent.sock;
+  DB(`${red}[${id}]${util.inspect(b,{depth:1000})}${off}`);
+  //DB(b.hex())
+  agent.buffer = Buffer.concat([agent.buffer,b]);
+  consume(agent);
 }
 
-var yeld = false; // When true, the state machine loop shoudl return until called with more buffer.
-var state = 0;
-
-function consume (id, s) { // id socket
-  var statein = state, msg;
-  //DB("\x1b[33mconsume " + id + " " + state + "\x1b[0m");
-  while (!yeld) {
-    //DB("\x1b[1;30m[SM " + state + "]\x1b[0m");
-    switch (state) {
+function consume (a) { // agent
+  var msg;
+  while (!a.yeld) {
+    DB(`${yellow}[${a.id}:${a.state}]${util.inspect(a.buffer)}${off}`);
+    switch (a.state) {
     case 0: // READ HEADERS AND UPDATE WEBSOCKET PROTOCOL
-      consumeHttpHeaders(id, s);
+      consumeHttpHeaders(a);
       break;
      case 1:
-      s.write(Uint8Array.of(0x82, 0x02, 0, id), "binary")
-      s.write("\x81\x11Welcome To Server", "binary")
-      s.write("\x89\x08Welcome!", "binary");
-      state = 2;
+      a.sock.write(Uint8Array.of(0x82, 0x02, 0, a.id), "binary")
+      a.sock.write("\x81\x11Welcome To Server", "binary")
+      a.sock.write("\x89\x08Welcome!", "binary");
+      a.state = 2;
     case 2: // DETERMINE NEXT MESSAGE
-      getNextState(id);
+      getNextState(a);
       break;
     case 3:
-      msg = consumeMsgText(id);
-      if (!yeld) { consumeDecodedMsg(msg, s); }
+      msg = consumeMsgText(a);
+      if (!a.yeld) { consumeDecodedMsg(msg, a); }
       break;
     case 4:
-      consumeMsgPong(id);
+      consumeMsgPong(a);
       break;
     case 5:
-      msg = consumeMsgBinary(id);
-      if (!yeld) { consumeDecodedMsgBinary(msg, s); }
+      msg = consumeMsgBinary(a);
+      if (!a.yeld) { consumeDecodedMsgBinary(msg, a); }
       break;
     case 88:
-      msg = consumeMsgText(id);
-      if (!yeld) { state = 99; } // Close connection so we're done.
+      msg = consumeMsgText(a);
+      if (!a.yeld) { a.state = 99; } // Close connection so we're done.
       break;
     case 99:
-      yeld = true;
+      a.yeld = true;
     }
   }
-  yeld = false;
+  a.yeld = false;
 }
 
-function getNextState (id) {
-  if (1 <= buffers.length) {
-    switch (buffers[0]) {
+function getNextState (a) {
+  if (1 <= a.buffer.length) {
+    switch (a.buffer[0]) {
     case 0x81 :
-      state = 3; // Text
+      a.state = 3; // Text
       break;
     case 0x82 :
-      state = 5; // Binary
+      a.state = 5; // Binary
       break;
     case 0x8a :
-      state = 4; // PONG
+      a.state = 4; // PONG
       break;
     case 0x88 :
-      state = 88; // Closing
+      a.state = 88; // Closing
       break;
     default :
-      state = 99;
+      a.state = 99;
       break;
     }
   } else {
-    yeld = true;
+    a.yeld = true;
   }
 }
 
@@ -144,99 +161,108 @@ function writeStreamBinary (msg, s) {
   s.write(buff, "binary");
 }
 
-function consumeDecodedMsgBinary (msg, s) {
-  //DB("[bin-dec]" + msg.readUInt8(0) + " " + msg.readUInt8(1) + " " + msg.readFloatBE(2) + " " + msg.readFloatBE(6) + " " + msg.readFloatBE(10));
-  writeStreamBinary(msg, s);
+function consumeDecodedMsgBinary (msg, a) { // Buff Agent
+  writeStreamBinary(msg, a.sock);
 }
 
-function consumeDecodedMsg (msg, s) {
+function consumeDecodedMsg (msg, a) {
   //var msgs = msg.split(' ')
   //if (msgs[0] == 'l') { writeStream(msgs[2] + " " + msgs[3] + " " + msgs[4], s); }
 }
 
 // Doesn't consume buffer until entire pong command is evaluated.
-function consumeMsgText (id) {
-  if (buffers.length < 2 ) { yeld = true; return; }
+function consumeMsgText (a) {
+  if (a.buffer.length < 2 ) { a.yeld = true; return; }
 
-  var ml = buffers[1]
+  var ml = a.buffer[1]
   var mask = ml & 0b10000000;
   var len  = ml & 0b01111111;
-  if (125 < len) { state = 99; return; } // TODO Handle extended payload langths.
+  if (125 < len) { a.state = 99; return; } // TODO Handle extended payload langths.
 
-  if (buffers.length < (4 + len)) { yeld = true; return; }
-  var mask = buffers.slice(2,6); // mask index and mask array
-  var msg = buffers.slice(6,6+len).map((c,i,b)=>c^buffers[2+i%4]).toString();
+  if (a.buffer.length < (4 + len)) { a.yeld = true; return; }
+  var mask = a.buffer.slice(2,6); // mask index and mask array
+  var msg = a.buffer.slice(6,6+len).map((c,i,b)=>c^a.buffer[2+i%4]).toString();
   DB("[text]"+msg);
-  global.msg=msg
-  buffers = buffers.slice(6+len); // pop what we just consumed
-  state = 2;
+  a.buffer = a.buffer.slice(6+len); // pop what we just consumed
+  a.state = 2;
   return msg;
 }
 
-function consumeMsgBinary (id) {
-  if (buffers.length < 2 ) { yeld = true; return; }
+function consumeMsgBinary (a) {
+  if (a.buffer.length < 2 ) { a.yeld = true; return; }
 
-  var ml = buffers[1]
+  var ml = a.buffer[1]
   var mask = ml & 0b10000000;
   var len  = ml & 0b01111111;
-  if (125 < len) { state = 99; return; } // TODO Handle extended payload langths.
+  if (125 < len) { a.state = 99; return; } // TODO Handle extended payload langths.
 
-  if (buffers.length < (4 + len)) { yeld = true; return; }
-  var mask = buffers.slice(2,6); // mask index and mask array
-  var msg = buffers.slice(6,6+len).map((c,i,b)=>c^buffers[2+i%4])
+  if (a.buffer.length < (4 + len)) { a.yeld = true; return; }
+  var mask = a.buffer.slice(2,6); // mask index and mask array
+  var msg = a.buffer.slice(6,6+len).map((c,i,b)=>c^a.buffer[2+i%4])
   //DB("[bin]"+msg);
-  buffers = buffers.slice(6+len); // pop what we just consumed
-  state = 2;
+  a.buffer = a.buffer.slice(6+len); // pop what we just consumed
+  a.state = 2;
   return msg;
 }
 
 // Doesn't consume buffer until entire pong command is evaluated.
-function consumeMsgPong (id) {
-  if (buffers.length < 2) { yeld = true; return; }
+function consumeMsgPong (a) {
+  if (a.buffer.length < 2) { a.yeld = true; return; }
 
-  var d = buffers[1]
+  var d = a.buffer[1]
   var mask = d & 0x80;
   var len = d & 0x7f;
-  if (125 < len) { state = 99; return; } // TODO Handle extended payload lengths.
+  if (125 < len) { a.state = 99; return; } // TODO Handle extended payload lengths.
 
-  if ( buffers.length < (4 + len)) { yeld = true; return; } // Full message not available yet.
+  if ( a.buffer.length < (4 + len)) { a.yeld = true; return; } // Full message not available yet.
 
-  var mask = buffers.slice(2,6); // mask index and mask array
-  DB("[pong]"+buffers.slice(6,6+len).map((c,i)=>c^buffers[2+i%4]).toString());
-  buffers = buffers.slice(6+len); // pop what we just consumed
-  state = 2;
+  var mask = a.buffer.slice(2,6); // mask index and mask array
+  DB("[pong]"+a.buffer.slice(6,6+len).map((c,i)=>c^a.buffer[2+i%4]).toString());
+  a.buffer = a.buffer.slice(6+len); // pop what we just consumed
+  a.state = 2;
 };
 
 
-function consumeHttpHeaders (id, s) {
+function consumeHttpHeaders (a) { // Agent
   var next=0, from=0, to, p, header;
   while (true) {
-    to = buffers.indexOf('\n', from); // Look for newline
-    if (-1 == to) { yeld = true; break; } // Buffer incomplete, return later
+    to = a.buffer.indexOf('\n', from); // Look for newline
+    if (-1 == to) { a.yeld = true; break; } // Buffer incomplete, return later
     // Found full header
     next = to+1; // Next position to start scanning from next time around.
-    if (13 == buffers[to-1]) { --to; } // Ignore newline and this possible return char when slicing the header.
+    if (13 == a.buffer[to-1]) { --to; } // Ignore newline and this possible return char when slicing the header.
     if (from == to) { // Empty line...done scanning httpHeaders.
-      websocketSwitchProtocol(s);
-      state = 1;
+      websocketSwitchProtocol(a);
+      a.state = 1;
       break;
     }
     //header = buffers.slice(from,to); // Save header
-    p = buffers.indexOf(' ', from);
-    httpHeaders[buffers.slice(from,p).toString()] = buffers.slice(p+1,to).toString() ;
+    p = a.buffer.indexOf(' ', from);
+    a.headers[a.buffer.slice(from,p).toString()] = a.buffer.slice(p+1,to).toString() ;
     from = next;
   }
-  buffers = buffers.slice(next); // Throw away everything we've successfully scanner so far.
+  a.buffer = a.buffer.slice(next); // Throw away everything we've successfully scanner so far.
 };
 
 
-function websocketSwitchProtocol (s) {
-  DB(httpHeaders);
+function websocketSwitchProtocol (a) {
+  DB(JSON.stringify(a.headers));
   //global.httpHeaders = httpHeaders; // Debugging: For some reason var with "use strict" does not result in it being in TGE.
-  var txt = httpHeaders['Sec-WebSocket-Key:'] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+  var txt = a.headers['Sec-WebSocket-Key:'] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
   var dig = Crypto.createHash('sha1').update(txt).digest().toString('base64')
-  s.write("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "+dig+"\r\n\r\n", "binary");
+  a.sock.write("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "+dig+"\r\n\r\n", "binary");
 };
+
+//////////////
+// fun
+/////////////
+var d = Date.now();
+var b = 0;
+var a = Float64Array.from([1.1,2.1,3.1,4.1,5.1,6.1,7.1,8.1,9.1,10.1]);
+//for (var i=0; i<65536*16384; i++) b += a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9];
+//for (var i=0; i<65536*16384; i++) b+=  a[i];
+//console.log(Date.now() - d + " " + b);
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -246,6 +272,7 @@ const Repl  = require('repl');
 const qrepl = Repl.start({prompt:'REPL>', useGlobal:true, replMode:Repl.REPL_MODE_SLOPPY});
 qrepl.on('exit', ()=>process.exit());
 
+server();
 
 ////////////////////////////////////////////////////////////////////////////////
 // TODO
@@ -283,5 +310,10 @@ let implies IIFE immediate invoked function expression
 
 # Float32Array
  a typed array 
+
+# String.prototype.hex = function(){ return [...this].map(c=> ("0"+c.charCodeAt().toString(16)).slice(-2)).join(); }
+# String.fromCharCode(...[65,66,67])
+# Buffer("\uffff").toString().split('').map( c=>c.charCodeAt() )
+
 
 *******************************************************************************/
